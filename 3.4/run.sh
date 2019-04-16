f470656e9279a248e7d68772f16340d15e273d61
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-16 20:33:38 +0900

    Modify mount directory

--- 3.4/run.sh
+++ 3.4/run.sh
@@ -2,7 +2,7 @@
 
 docker rm haxe3.4
 docker run --name "haxe3.4" \
-           -it -v ${HOME}/workspace/develop/learning-haxe:/home/builder/workspace \
+           -it -v ${HOME}/workspace/develop/:/home/builder/workspace \
            -e LOCAL_UID=$(id -u $USER) \
            -e LOCAL_GID=$(id -g $USER) \
            nabezokodaikon/haxe3.4 zsh
